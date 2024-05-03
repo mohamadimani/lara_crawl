88bd84cf971cd4fbc1e2dad15c8e13d311b4eb58
manimohamadi <mohamadimahdi@yahoo.com>
2024-05-04 02:26:51 +0330

    +

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use jcobhams\NewsApi\NewsApi;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-
-
-route::get('/', function () {
-
-
-    $newsapi = new NewsApi("da89dedff760447f83ace753b7e0bd6d");
-
-
-    $top_headlines = $newsapi->getTopHeadlines('money');
-
-
-
-    dd($top_headlines);
-});
